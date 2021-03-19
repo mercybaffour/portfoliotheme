db6e7b39bb5dc9f3d2f61f8ddd284d28fe284332
Mercy Baffour <mercy.baffour@gmail.com>
2021-03-19 15:36:59 -0400

    initial styling for home page and added templates up to home.php

---
+++ header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@500&family=Montserrat&display=swap" rel="stylesheet">
+    <?php wp_head(); ?>
+  </head>
+  <body <?php body_class(); ?>>
+
+    <div id="page">
+
+      <a href="#content" class="skip-link screen-reader-text">
+        <?php esc_html_e( 'Skip to content', 'mercy' ); ?>
+      </a>
+
+      <header id="masthead" class="site-header" role="banner">
+
+        <nav id="site-navigation" class="main-navigation" role="navigation">
+          <?php
+            $args = [
+              'theme_location' => 'main-menu'
+            ];
+            wp_nav_menu( $args );
+          ?>
+        </nav>
+
+        <div class="site-branding">
+          <p class="site-title">
+            <a href="<?php echo esc_url( home_url( '/' ) ) ;?>" rel="home">
+              <?php bloginfo( 'name' ); ?>
+            </a>
+          </p>
+          <p class="site-description" >
+            <?php bloginfo( 'description' ); ?>
+          </p>
+        </div>
+
+
+
+      </header>
+
+      <div id="content" class="site-content">
