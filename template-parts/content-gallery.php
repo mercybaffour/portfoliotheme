7bd3cd9f4872b694b4eb3cd6e3d52a28e5583204
Mercy Baffour <mercy.baffour@gmail.com>
2021-03-22 11:53:46 -0400

    additional styling

--- template-parts/content-gallery.php
+++ template-parts/content-gallery.php
@@ -5,8 +5,6 @@
 
             <span class="dashicons dashicons-format-<?php echo get_post_format( $post->ID ); ?>"></span>
 
-            <p><?php esc_html_e( 'Enjoy this gallery post!!!', 'mercy' ) ?></p>
-
             <?php the_title( '<h1>', '</h1>' ); ?>
 
         </header>
